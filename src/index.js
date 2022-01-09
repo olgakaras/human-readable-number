42e65bffb4951e924be9c57058e2f3294239a41c
olgakaras <olga.karas123@gmail.com>
2022-01-09 18:11:20 +0300

    feat: add for numbers up to 99

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function toReadable (number) {
-  
+  let arrNum = Array.from(String(number), Number);
+  let a = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eihteen', 'nineteen', 'twenty'];
+  let b = ['thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+  let c = ' hundred ';
+
+  let strDecimalNum = b[arrNum[0] - 3] + ' ' + a[arrNum[1]];
+
+  if(number < 100) {
+    if(number <= 20) {
+      return a[number];
+    }
+    else if(arrNum[1] == 0) {
+      console.log(b[arrNum[0] - 2]);
+    }
+    else {
+      return strDecimalNum;
+    }
+  }
+  else {
+    return a[arrNum[0]] + c + strDecimalNum;
+  }
 }
