983947f75249de540e04cfac827d23c08611a7c6
olgakaras <olga.karas123@gmail.com>
2022-01-09 18:53:19 +0300

    fix: change function

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 module.exports = function toReadable (number) {
   let arrNum = Array.from(String(number), Number);
-  let a = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eihteen', 'nineteen', 'twenty'];
-  let b = ['thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
-  let c = ' hundred ';
+  let a = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eihteen', 'nineteen'];
+  let b = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+  let c = ' hundred';
 
-  let strDecimalNum = b[arrNum[0] - 3] + ' ' + a[arrNum[1]];
+  let strDecimalNum = b[arrNum[0] - 2] + ' ' + a[arrNum[1]];
 
   if(number < 100) {
-    if(number <= 20) {
+    if(number <= 19) {
       return a[number];
     }
     else if(arrNum[1] == 0) {
@@ -17,7 +17,21 @@ module.exports = function toReadable (number) {
       return strDecimalNum;
     }
   }
-  else {
-    return a[arrNum[0]] + c + strDecimalNum;
-  }
+  else if (number < 1000) {
+    if (arrNum[1] == 0 && arrNum[2] == 0) {
+      return a[arrNum[0]] + c;
+    }
+    else if(number%100 <= 19) {
+      return a[arrNum[0]] + c + " " + a[number%100];
+    }
+    else {
+      if(arrNum[2] == 0) {
+        return a[arrNum[0]] + c + " " + b[arrNum[1] - 2];
+      }
+      // else {
+      //   return a[arrNum[0]] + c + " ";
+      // }
+      
+    }
+  } 
 }
